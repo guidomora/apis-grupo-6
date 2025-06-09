17af39ac188e8399c842f9a5b4759d4c433257f5
guido morabito <guidomorabito161@hotmail.com>
2025-06-08 21:01:57 -0300

    archivo server eliminado

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerSpec = require('./src/docs/swagger');
-// const userRoutes = require('./src/routes/users/routes');
-// const trainerRoutes = require('./src/routes/trainers/routes');
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(express.json());
-// app.use('/api/users', userRoutes);
-// app.use('/api/trainers', trainerRoutes);
-// app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-const Server = require("./src/models/server");
-
 require("dotenv").config();
 
-const server = new Server();
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("./src/database/config");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./src/docs/swagger");
 
-server.listen();
+const userRoutes = require("./src/routes/users/routes");
+const trainerRoutes = require("./src/routes/trainers/routes");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Conectar la base de datos
+(async () => {
+  try {
+    await dbConnection();
+    console.log("Base de datos conectada");
+  } catch (error) {
+    console.error("Error al conectar la base de datos:", error);
+    process.exit(1);
+  }
+})();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use("/api/users", userRoutes);
+app.use("/api/trainers", trainerRoutes);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
